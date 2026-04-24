a4a7552ea605670ff0b7b14f34002f434e8bc018
kphuu <kphung92@gmail.com>
2026-04-24 04:38:55 -0500

    first commit

---
+++ arb-tool/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+
+const SPORTS = [
+  'baseball_mlb',
+  'basketball_nba',
+  'icehockey_nhl',
+  'americanfootball_nfl',
+  'soccer_usa_mls'
+];
+
+app.get('/api/odds', async (req, res) => {
+  try {
+    const promises = SPORTS.map(sport =>
+      fetch(`https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${process.env.ODDS_API_KEY}&regions=us&markets=h2h&oddsFormat=american`)
+        .then(r => r.json())
+        .catch(() => [])
+    );
+    const results = await Promise.all(promises);
+    const allGames = results.flat().filter(g => g && g.id);
+    res.json(allGames);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => console.log('Running on http://localhost:3000'));
