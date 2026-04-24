bd3dca5ad028f410e24e44f0b021d5eaf17fdb8d
kphuu <kphung92@gmail.com>
2026-04-24 05:05:27 -0500

    add pinnacle sharp line

--- arb-tool/server.js
+++ arb-tool/server.js
@@ -8,13 +8,13 @@ const SPORTS = [
   'baseball_mlb',
   'basketball_nba',
   'icehockey_nhl',
-  'americanfootball_nfl',
+  'americanfootball_nfl'
 ];
 
 app.get('/api/odds', async (req, res) => {
   try {
     const promises = SPORTS.map(sport =>
-      fetch(`https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${process.env.ODDS_API_KEY}&regions=us&markets=h2h&oddsFormat=american`)
+      fetch(`https://api.the-odds-api.com/v4/sports/${sport}/odds/?apiKey=${process.env.ODDS_API_KEY}&regions=us,eu&markets=h2h&oddsFormat=american&bookmakers=fanduel,draftkings,betmgm,betrivers,caesars,fanatics,pinnacle`)
         .then(r => r.json())
         .catch(() => [])
     );
